77e0c0c3294113fe5a8747b5f19d15257cb9ed95
Ohdeathbruh <93175070+Ohdeathbruh@users.noreply.github.com>
2021-10-27 09:05:13 +0700

    Update TestScript.lua

--- TestScript.lua
+++ TestScript.lua
@@ -35,25 +35,25 @@ Invisible.TextColor3 = Color3.fromRGB(0, 0, 0)
 Invisible.TextSize = 14.000
 Invisible.MouseButton1Click:Connect(function()
 	script.Parent.MouseButton1Click:Connect(function()
-		game.Players.LocalPlayer.PlayerGui.ScreenGui.Frame.Visibleai.Visible = true
-		game.Players.LocalPlayer.PlayerGui.ScreenGui.Frame.Invisible.Visible = false
-		game.Players.LocalPlayer.Character.LowerTorso.Transparency = 1
-		game.Players.LocalPlayer.Character.UpperTorso.Transparency = 1
-		game.Players.LocalPlayer.Character.LeftHand.Transparency = 1
-		game.Players.LocalPlayer.Character.LeftLowerArm.Transparency = 1
-		game.Players.LocalPlayer.Character.LeftUpperArm.Transparency = 1
-		game.Players.LocalPlayer.Character.RightUpperArm.Transparency = 1
-		game.Players.LocalPlayer.Character.RightLowerArm.Transparency = 1
-		game.Players.LocalPlayer.Character.RightHand.Transparency = 1
-		game.Players.LocalPlayer.Character.Head.Transparency = 1
-		game.Players.LocalPlayer.Character.RightUpperLeg.Transparency = 1
-		game.Players.LocalPlayer.Character.RightLowerLeg.Transparency = 1
-		game.Players.LocalPlayer.Character.RightFoot.Transparency = 1
-		game.Players.LocalPlayer.Character.LeftUpperLeg.Transparency = 1
-		game.Players.LocalPlayer.Character.LeftLowerLeg.Transparency = 1
-		game.Players.LocalPlayer.Character.LeftFoot.Transparency = 1
-		game.Players.LocalPlayer.Character.Head.face.Transparency = 1
-		game.Players.LocalPlayer.Character.Head.face.Transparency = 1
+		game.Players.bendyinyetiyo.PlayerGui.ScreenGui.Frame.Visibleai.Visible = true
+		game.Players.bendyinyetiyo.PlayerGui.ScreenGui.Frame.Invisible.Visible = false
+		game.Players.bendyinyetiyo.Character.LowerTorso.Transparency = 1
+		game.Players.bendyinyetiyo.Character.UpperTorso.Transparency = 1
+		game.Players.bendyinyetiyo.Character.LeftHand.Transparency = 1
+		game.Players.bendyinyetiyo.Character.LeftLowerArm.Transparency = 1
+		game.Players.bendyinyetiyo.Character.LeftUpperArm.Transparency = 1
+		game.Players.bendyinyetiyo.Character.RightUpperArm.Transparency = 1
+		game.Players.bendyinyetiyo.Character.RightLowerArm.Transparency = 1
+		game.Players.bendyinyetiyo.Character.RightHand.Transparency = 1
+		game.Players.bendyinyetiyo.Character.Head.Transparency = 1
+		game.Players.bendyinyetiyo.Character.RightUpperLeg.Transparency = 1
+		game.Players.bendyinyetiyo.Character.RightLowerLeg.Transparency = 1
+		game.Players.bendyinyetiyo.Character.RightFoot.Transparency = 1
+		game.Players.bendyinyetiyo.Character.LeftUpperLeg.Transparency = 1
+		game.Players.bendyinyetiyo.Character.LeftLowerLeg.Transparency = 1
+		game.Players.bendyinyetiyo.Character.LeftFoot.Transparency = 1
+		game.Players.bendyinyetiyo.Character.Head.face.Transparency = 1
+		game.Players.bendyinyetiyo.Character.Head.face.Transparency = 1
 	end)
 
 
